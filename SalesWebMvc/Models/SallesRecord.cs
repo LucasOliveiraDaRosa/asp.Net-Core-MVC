c66e6b431ee17bf835822aa39271d842daa09848
Lucas Rosa <lucasdarosasd@gmail.com>
2021-12-16 01:13:18 -0300

    Add New migration OthersMigrations

---
+++ SalesWebMvc/Models/SallesRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SallesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SallesRecord()
+        {
+
+        }
+
+        public SallesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SalesWebMvc.Models.Enums;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BithDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SallesRecord> Salles { get; set; } = new List<SallesRecord>();
+
+        public Seller()
+        {
+
+        }
+        public Seller(int id, string name, string email, DateTime bithDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BithDate = bithDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SallesRecord sr)
+        {
+            Salles.Add(sr);
+        }
+
+        public void RemoveSales(SallesRecord sr)
+        {
+            Salles.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Salles.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
