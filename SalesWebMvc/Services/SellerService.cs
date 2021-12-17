326dc5ce9eeeb449c3d0416b84a38c96f240bb5c
Lucas Rosa <lucasdarosasd@gmail.com>
2021-12-16 21:50:21 -0300

    Create service Seller

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> findAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
