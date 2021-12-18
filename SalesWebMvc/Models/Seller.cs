2adcf80d84ecc3864dde7e10f02a58a8163096ec
Lucas Rosa <lucasdarosasd@gmail.com>
2021-12-17 23:41:24 -0300

    validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -12,15 +12,22 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         [Display(Name = "Nome")]
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60,MinimumLength = 3,ErrorMessage = "{0} size should be between {2} and {1}.")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage ="Enter a valid email")]
         public string Email { get; set; }
         [Display(Name = "Data de aniversário")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} required")]
         public DateTime BithDate { get; set; }
         [Display(Name = "Salário")]
         [DisplayFormat( DataFormatString ="{0:F2}")]
+        [Range(100.00,5000.0,ErrorMessage ="{0} must be from {1} to {2}")]
+        [Required(ErrorMessage = "{0} required")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         [Display(Name = "Departamento")]
